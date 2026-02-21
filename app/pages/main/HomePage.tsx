96fd6b0807f5b9c3e6a3bc09563e0924ecd08124
archiProg <baramee.panarchi@gmail.com>
2026-02-21 15:02:44 +0700

    add: Resume Case Banner

--- app/pages/main/HomePage.tsx
+++ app/pages/main/HomePage.tsx
@@ -1,10 +1,12 @@
 import ActionCard from "@/components/ActionCard";
 import { CARD, TEXT_SIZE } from "@/constants/styles";
 import Provider from "@/services/providerService";
+import { RootState } from "@/store";
 import { FontAwesome } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { useTranslation } from "react-i18next";
 import { Image, ScrollView, Text, TouchableOpacity, View, useColorScheme } from "react-native";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
   const token = Provider.Token;
@@ -12,6 +14,8 @@ const HomePage = () => {
 
   const router = useRouter();
   const { t } = useTranslation();
+
+  const consultInfo = useSelector((state: RootState) => state.recall.consultInfo);
 
   return (
     <View className="flex-1 w-full items-center justify-center">
@@ -84,15 +88,60 @@ const HomePage = () => {
             </View>
           </View>
 
+
+
           {/* Action Card */}
           <ActionCard
             title={t("search-doctor")}
             description={t("search-doctor-description")}
             textButton={t("search")}
             onPress={() => { router.push("/pages/main/DoctorMatchStepPage") }} />
+
+
+          {/* Resume Case Banner */}
+          {consultInfo && (
+            <View className="mt-4">
+              <Text className={`${CARD.title} text-black dark:text-white mb-1`}>
+                {t("home-screen-incomplete-consultation")}
+              </Text>
+              <View className={`${CARD.body} bg-yellow-50 border-yellow-200 dark:bg-yellow-900 dark:border-yellow-800 shadow-md mt-2`}>
+                <View className="flex-row items-center justify-between">
+                  <View className="flex-1">
+                    <Text className={`${CARD.title} text-black dark:text-white mb-1`}>
+                      Doctor: {consultInfo.doctorInfo.name}
+                    </Text>
+
+                    <Text className={`${CARD.subtitle} text-gray-600 dark:text-gray-300`}>
+                      Specialty: {consultInfo.doctorInfo.specialization_name}
+                    </Text>
+                  </View>
+
+
+                </View>
+                <View className="flex-row items-center justify-end">
+                  <TouchableOpacity
+                    className="bg-blue-500 px-4 py-2 rounded-xl"
+                    onPress={() =>
+                      router.replace({
+                        pathname: "/pages/main/PreCallPage",
+                        params: {
+                          consultId: consultInfo.caseId
+                        },
+                      })
+                    }
+                  >
+                    <Text className="text-white font-semibold">
+                      {t("resume")}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          )}
+
         </ScrollView>
-      </View>
-    </View>
+      </View >
+    </View >
   );
 };
 
